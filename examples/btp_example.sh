7e79d234d2d4940377fb62589391ca8b1217424a
b avants <stnava@gmail.com>
2012-04-23 17:50:28 -0400

    ENH: btp example

--- examples/btp_example.sh
+++ examples/btp_example.sh
@@ -1,6 +1,6 @@
 
 mkdir templateex
 cd templateex 
-ln -s ../data/phantom* . 
-buildtemplateparallel.sh -d 2 -o PH -c 0 -m CC -t GR  phantom*jpg
+ln -s ../data/phantom* .  #fortex
+buildtemplateparallel.sh -d 2 -o PH -c 0 -m CC -t GR  phantom*jpg #fortex
 
