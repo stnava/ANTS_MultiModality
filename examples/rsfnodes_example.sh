8f81713128d769c20495aa2726719dc46bcafa5f
b avants <stnava@gmail.com>
2012-04-23 15:16:25 -0400

    ENH: adding segmentation

--- examples/rsfnodes_example.sh
+++ examples/rsfnodes_example.sh
@@ -1,9 +1,14 @@
 
 img=data/rsfmri.nii.gz
 out=output/rsfmri
-if [[ -s ${out}_avg.nii.gz ]] ; then 
-  ThresholdImage 3 ${out}_avg.nii.gz  ${out}_bmask.nii.gz 300 9999 #fortex
+if [[ -s ${out}avg.nii.gz ]] ; then 
+  ThresholdImage 3 ${out}avg.nii.gz  ${out}_bmask.nii.gz 300 9999 #fortex
   ImageMath 3 ${out}_bmask.nii.gz GetLargestComponent ${out}_bmask.nii.gz #fortex
+  N3BiasFieldCorrection 3 ${out}avg.nii.gz ${out}n3.nii.gz 2 ${out}_bmask.nii.gz
+  N3BiasFieldCorrection 3  ${out}n3.nii.gz ${out}n3.nii.gz 1 ${out}_bmask.nii.gz
+  Atropos -d 3 -a ${out}n3.nii.gz -a ${out}compcorr_variance.nii.gz -m [0.25,1x1x1] -o [${out}seg.nii.gz,${out}prob%02d.nii.gz] -x ${out}_bmask.nii.gz -c [5,0] -i kmeans[3]
+  Atropos -d 3 -a ${out}n3.nii.gz -a ${out}compcorr_variance.nii.gz -m [0.25,1x1x1] -o [${out}seg.nii.gz,${out}prob%02d.nii.gz] -x ${out}_bmask.nii.gz -c [5,0] -i priorprobabilityimages[3,${out}prob%02d.nii.gz,0]
+  ThresholdImage 3 ${out}seg.nii.gz ${out}_bmask.nii.gz 3 3 
   ImageMath 4 ${out}compcorr.nii.gz CompCorrAuto ${out}.nii.gz ${out}_bmask.nii.gz 6 #fortex
   sccan --timeseriesimage-to-matrix [ ${out}compcorr_corrected.nii.gz , ${out}_bmask.nii.gz , 0 , 1.0 ] -o ${out}.csv #fortex
   sccan --svd sparse[ ${out}.csv ,  ${out}_bmask.nii.gz , -0.05 ] -n 40 -i 20 --PClusterThresh 50 -o ${out}RSFNodes.nii.gz  #fortex
