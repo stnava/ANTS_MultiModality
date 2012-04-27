5c099509d7bf8359a87ec2bbf477e6ee5e68181e
brian avants <stnava@gmail.com>
2012-04-27 12:25:55 -0400

    ENH: presented 4/27/2012

--- examples/btp3_example.sh
+++ examples/btp3_example.sh
@@ -2,7 +2,7 @@ if [[ -d templateex ]] ; then
   cd templateex 
   ct=10
   echo vol > globalvols.csv 
-  for x in ../data/phantom*wmgm.jpg ; do #fortex
+  for x in ../data/phantom*wmgm.jpg ; do 
     ThresholdImage 2 $x mask.nii.gz 90 1.e9 > temp
     Atropos -d 2 -a $x -m [0.1,1x1] -i kmeans[2] -x mask.nii.gz -o [temp${ct}seg.nii.gz] -c [3,0] > temp
     vol=`ImageMath 2 temp LabelStats temp${ct}seg.nii.gz  temp${ct}seg.nii.gz | grep "Label 1" `
@@ -12,15 +12,24 @@ if [[ -d templateex ]] ; then
   done
   ct=10 
   if [[ -s PHtemplateseg.nii.gz ]] ; then 
-    ThresholdImage 2 PHtemplateseg.nii.gz  mask.nii.gz 127 300  # fortex
+    ThresholdImage 2 PHtemplateseg.nii.gz  maskg.nii.gz 127 127  # fortex
+    ThresholdImage 2 PHtemplateseg.nii.gz  maskw.nii.gz 255 255 
 #    ImageMath 2 mask.nii.gz ME mask.nii.gz 1 
-    for x in PHphantom*wmgmWarp.nii.gz ; do 
-      ANTSJacobian 2 $x phantom${ct} 1 mask.nii.gz 0  # fortex
-      SmoothImage 2 phantom${ct}logjacobian.nii.gz 1.0  phantom${ct}logjacobian.nii.gz #fortex
+    for x in PHphantom*wmgmWarp.nii.gz ; do  #fortex
+      ANTSJacobian 2 $x phantomW${ct} 1 maskw.nii.gz 0  
+      ANTSJacobian 2 $x phantomG${ct} 1 maskg.nii.gz 0  # fortex
+#      SmoothImage 2 phantom${ct}logjacobian.nii.gz 1.0  phantom${ct}logjacobian.nii.gz #fortex
       let ct=$ct+1
     done 
-    ls phantom*logjacobian.nii.gz > list.txt   # fortex
-    sccan --imageset-to-matrix [list.txt,mask.nii.gz] -o phantomlogjacs.csv   # fortex
+    ls phantomG*logjacobian.nii.gz > list.txt   # fortex
+    sccan --imageset-to-matrix [list.txt,maskg.nii.gz] -o phantomGlogjacs.csv   # fortex
+    ls phantomW*logjacobian.nii.gz > list.txt   
+    sccan --imageset-to-matrix [list.txt,maskw.nii.gz] -o phantomWlogjacs.csv   
+    # Now run SCCAN to get multivariate statistics 
+    sccan --scca two-view[phantomGlogjacs.csv,globalvols.csv,maskg.nii.gz, NA ,-0.1,-1] -o CCA.nii.gz -n 1 -p 1000 -i 10 --PClusterThresh 100 # fortex
+#    sccan --scca two-view[phantomWlogjacs.csv,phantomGlogjacs.csv,maskw.nii.gz,maskg.nii.gz,-0.1,-0.1] -o CCA.nii.gz -n 3 -p 100  --PClusterThresh 100 # fortex
+#  0.363314 p-value 0.002997 ct 1000 true 0.978359 # fortex
+
   fi
 else
   exit 1
@@ -28,32 +37,3 @@ fi
 
 exit
 
-predictor<-read.csv("data/phantpredictors.csv")
-predictor<-read.csv("templateex/globalvols.csv")
-logjac<-read.csv("templateex/phantomlogjacs.csv")
-
-attach( logjac ) ; attach( predictor )
-
-nvox<-ncol(logjac)
-
-pvals<-rep(NA,nvox)
-
-for ( x in c(1:nvox) ) 
-{ 
-
-  voxels<-logjac[,x]
-
-  lmres<-summary(lm( voxels ~  vol ))
-
-  coeff<-coefficients( lmres )
-
-  pval<-coeff[2,4]
-
-  pvals[x]<-pval
-
-}
-qvals<-p.adjust(pvals)
-print(min(qvals))
-write.csv(1-qvals,'templateex/qvals.csv')
-
- sccan --vector-to-image [ templateex/qvals.csv , templateex/mask.nii.gz , 1] -o temp.nii.gz
